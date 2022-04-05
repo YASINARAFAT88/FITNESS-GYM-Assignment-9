5b44d8e571eaba723028cf0ade3fc9e34d4548f9
HP 840 <yasin80545859@.com>
2022-04-05 12:31:22 +0600

    small problem fixed in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './Home.css'
 import logo from './gym-benner.jpg'
 import { useNavigate } from 'react-router-dom';
-
+import { HooksContext } from '../../App';
 
 
 const Home = () => {
     const naviGate = useNavigate()
-    const [reviews, setReviews] = useState([]);
+    const reviews = useContext(HooksContext)
     
-    useEffect(() => {
-        fetch('data.json')
-            .then(res => res.json())
-            .then(data => setReviews(data))
-    }, [])
     return (
         <>
             <div className='home-container'>
 
                 <div className="home-detail">
                     <h1>Get to get good Health.<br /> Must Go gym Regularly!!</h1>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos laudantium animi magnam nostrum <br /> porro rem eligendi aperiam omnis provident nam eaque quam, dolore maxime deleniti nihil praesentium quasi, iste incidunt?</p>
+                    <p className='detail-text'>Are You Fit. Then You are a Hit. A Good helthful life Lead Do this then Work hard and Smartly or got to  gym Regularly First of all And Second Work Eight Hours Sleep and junk food not Allowd but week a one day consedareble!!</p>
                     <button>See Detail</button>
                     <u></u>
                 </div>
